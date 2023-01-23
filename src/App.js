32f56bec540ffe65c84ad7c6479270bcff548d79
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-01-23 21:25:25 +0000

    New commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-
 import Container from "./Components/Container";
 import Cookie from "./Components/Cookie";
 import Footer from "./Components/Footer";
 import Form from "./Components/Form";
-import Navigation from "./Components/Navigation";
-
-
+import Navigation from "./Components/NavBar/Navigation";
 
 function App() {
   return (
     <div className="body">
-     
-    <Navigation/>
-    <Form/>
-    <Container/>
-    <Cookie/>
-    <Footer/>
-     
+      <Navigation />
+      <Form />
+      <Container />
+      <Cookie />
+      <Footer />
     </div>
   );
 }
--- src/Pages/Listing.jsx
+++ src/Pages/Listing.jsx
@@ -1,32 +1,39 @@
-import React from 'react'
-import Navigation from '../Components/Navigation'
-import '../sass/Hero.scss'
+import React from "react";
+import Navigation from "../Components/NavBar/Navigation";
+import "../sass/Hero.scss";
 import "../sass/Form.scss";
-
 
 export default function Listing() {
   return (
     <>
-    <Navigation/>
-    <div className="hero">
-      <div className="hero__wrap">
-        <div className="form">
-          <div className="form__group">
-            <label className="form__label">What</label>
-            <input className="form__field" type="text" placeholder="Job title, skill or company"/>
-          </div>
+      <Navigation />
+      <div className="hero">
+        <div className="hero__wrap">
+          <div className="form">
+            <div className="form__group">
+              <label className="form__label">What</label>
+              <input
+                className="form__field"
+                type="text"
+                placeholder="Job title, skill or company"
+              />
+            </div>
 
-          <div class="form__group">
-            <label className="form__label">Where</label>
-            <input className="form__field" type="text" placeholder="Town, city or postcode"/>
-          </div>
+            <div class="form__group">
+              <label className="form__label">Where</label>
+              <input
+                className="form__field"
+                type="text"
+                placeholder="Town, city or postcode"
+              />
+            </div>
 
-          <div className="form__group">
-            <input className="form__btn" type="submit" value="Search"/>
+            <div className="form__group">
+              <input className="form__btn" type="submit" value="Search" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
